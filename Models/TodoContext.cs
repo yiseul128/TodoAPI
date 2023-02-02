8c9a9d62710e3102520ee6cdc4892834fd5c8d4f
yhlica <yli202@my.centennialcollege.ca>
2023-02-02 12:39:11 -0500

    Add project files.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace TodoAPI.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
